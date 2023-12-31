d5b73d06ca7390debda37125de885cf9a82d197a
riahifiras <120566908+riahifiras@users.noreply.github.com>
2023-12-31 15:17:55 +0100

    Add particle background effect.

--- src/containers/Front/Front.jsx
+++ src/containers/Front/Front.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import ParticleBackground from '../../components/ParticleBackground';
 import Header from '../Header/Header';
 import { me, image1, backgroundImg } from '../../components/Images';
 import './Front.css'
@@ -44,15 +45,20 @@ const Front = () => {
     backgroundPosition: 'center', // You can customize the background position as needed
   };
 
+    const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <>
       {!scrolling ? (
-        <div className='w-full h-[100vh] -mt-16 fixed' style={backgroundImageStyle}>
+        <div className='w-full h-[100vh] -mt-16 fixed'>
+          <ParticleBackground particlesLoaded={particlesLoaded}/>
           <div className='w-full h-screen translate-y-24 lg:translate-y-0 flex justify-between items-center flex-col-reverse lg:flex-row relative'>
             <div className='flex flex-col up lg:ml-36 items-center text-left lg:items-start w-3/4 lg:w-1/2'>
-              <h1 className='flex flex-col gap-4 lg:text-7xl text-3xl font-semibold font-euro'>Hi, I'm <br /><span className='whitespace-nowrap name'>Firas <span className='text-blue-600'>Riahi</span></span></h1>
+              <h1 className='flex flex-col gap-4 lg:text-7xl text-3xl font-semibold font-euro unselectable'>Hi, I'm <br /><span className='whitespace-nowrap name'>Firas <span className='text-blue-600'>Riahi</span></span></h1>
               <br />
-              <h3 className='lg:text-4xl text-xl'>Frontend developer, Game developer, and Graphic designer</h3>
+              <h3 className='unselectable lg:text-4xl text-xl'>Frontend developer, Game developer, and Graphic designer</h3>
               <button onClick={handleDownload} className='z-30 flex justify-center items-center h-10 w-36 mt-6 rounded-full bg-blue-700 text-white text-xl font-semibold hover:bg-blue-600 transition duration-200'>Download CV</button>
             </div>
             <img src={me} alt="" className='-translate-y-24 lg:-translate-y-0 absolute right-20 -bottom-16 size' />
