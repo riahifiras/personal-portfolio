f17809a6b4f7e3189800efb2f03988b2aeca5740
riahifiras <120566908+riahifiras@users.noreply.github.com>
2024-03-22 00:12:16 +0100

    Styled project popup background image.

--- src/components/ProjectPopup/ProjectPopup.jsx
+++ src/components/ProjectPopup/ProjectPopup.jsx
@@ -23,8 +23,9 @@ const ProjectPopup = ({ info, onClose }) => {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
             <div className="flex bg-white lg:flex-row flex-col lg:h-[540px] h-[80vh] lg:w-[1000px] w-[80%] rounded-lg" ref={popupRef}>
-                <div className="lg:h-540px h-[100%] w-full relative">
-                    <img className="lg:h-[540px] h-full object-cover lg:rounded-l-lg rounded-t-lg lg:w-[600px] w-full" src={info.image} alt="" />
+                <div className="relative lg:h-[540px] overflow-hidden h-[100%] w-full bg-transparent">
+                    <div style={{ '--image-url': `url(${info.image})` }} className="absolute h-[114%] -top-4 -left-4 w-[114%] bg-[image:var(--image-url)] bg-no-repeat bg-cover bg-center blur-md hue-rotate-0 backdrop-blur-0 backdrop-hue-rotate-0 shadow-none drop-shadow-none grayscale-0 sepia-0" />
+                    <img className="absolute lg:h-[540px] h-full object-contain lg:rounded-l-lg rounded-t-lg lg:w-[600px] w-full" src={info.image} alt="" />
                     <button
                         className="lg:hidden absolute right-4 top-4 flex justify-center items-center text-xl font-bold h-8 w-8 text-black rounded-full"
                         onClick={onClose}
@@ -32,6 +33,7 @@ const ProjectPopup = ({ info, onClose }) => {
                         <RxCross2 />
                     </button>
                 </div>
+
 
                 <div className={`w-full h-full lg:rounded-r-lg rounded-b-lg p-4 flex flex-col gap-8 ${darkMode ? "text-black" : "text-white"} ${darkMode ? "bg-[#cbd4d4]" : "bg-[#16181d]"}`}>
                     <div className="flex justify-between w-full">
